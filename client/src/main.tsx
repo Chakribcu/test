1db3bc6115e217f09a5f8b600e0cfc95b70a4e66
Chakridhar Jakkula <chakridhar.jakkula@mail.bcu.ac.uk>
2025-05-13 02:03:59 +0100

    Update main.tsx

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -4,7 +4,10 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
 
-const root = createRoot(document.getElementById("root")!);
+const rootElement = document.getElementById("root");
+if (!rootElement) throw new Error("Failed to find the root element");
+
+const root = createRoot(rootElement);
 
 root.render(
   <React.StrictMode>
